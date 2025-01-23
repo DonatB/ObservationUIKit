43f815357f2c3eb1005b3219b1b79b937f09120e
Donat Bajrami <donatbajrami@hotmail.com>
2025-01-23 10:19:22 +0100

    Initial Commit

---
+++ ObservationUIKit/ObservationUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationUIKitApp.swift
+//  ObservationUIKit
+//
+//  Created by Ptera on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ObservationUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
